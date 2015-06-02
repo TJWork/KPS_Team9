31716a9ca7fb6945be872ed3817d4989729762a7
TJWork <worktony@myvuw.ac.nz>
2015-06-02 15:36:36 +1200

    A fresh start

---
+++ src/domain/Mail.java
@@ -0,0 +1,65 @@
+package domain;
+
+public class Mail {
+
+    private String id;
+
+    private double weight;
+
+    private double volume;
+
+    private long time;
+
+    private Route route;
+
+    private double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
